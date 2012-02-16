c6661e6de8be4c74bf27351b8b34a37885e7c658
Jake Harding <jacob.s.harding@gmail.com>
2012-02-16 02:19:32 -0800

    Making completion bash-like

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# make completion feel BASH-like
+setopt BASH_AUTO_LIST
+setopt NO_AUTO_MENU
+setopt NO_ALWAYS_LAST_PROMPT
